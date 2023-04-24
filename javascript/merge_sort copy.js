c9c72584ed660cc0980d7e8c6e9326541359cfdd
Aruna <aruna.evan15@gmail.com>
2023-04-24 12:31:03 -0700

    solution

---
+++ javascript/merge_sort copy.js
@@ -0,0 +1,57 @@
+// merge 2 sorted arrays, only ever have to compare the first two elements of each 
+// can implement merge recursively as well :)
+function merge(a1, a2) {
+  const res = [];
+  let p1 = 0, p2 = 0;
+
+  while (a1[p1] && a2[p2]) {
+    if (a1[p1] < a2[p2]) {
+      res.push(a1[p1]);
+      p1++;
+    }
+    else if (a1[p1] >= a2[p2]) {
+      res.push(a2[p2]);
+      p2++;
+    }
+  }
+
+  if (a1[p1]) {
+    return [...res, ...a1.slice(p1)]
+  }
+  else if (a2[p2]) {
+    return [...res, ...a2.slice(p2)]
+  }
+  return res;
+}
+
+function mergeSort(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
+  else {
+    const len = arr.length;
+    const half = Math.floor(len/2);
+    return merge(mergeSort(arr.slice(0,half)), mergeSort(arr.slice(half)));
+  }
+}
+
+if (require.main === module) {
+  console.log("Expecting: [1, 2]");
+  console.log("=>", mergeSort([2, 1]));
+
+  console.log("");
+
+  console.log("Expecting: [1, 2, 3]");
+  console.log("=>", mergeSort([1, 2, 3]));
+
+  console.log("");
+
+  console.log("Expecting: [-10, 0, 2, 2, 5, 10, 20]");
+  console.log("=>", mergeSort([10, -10, 0, 2, 20, 5, 2]));
+}
+
+module.exports = mergeSort;
+
+/**
+ * 
+ */
